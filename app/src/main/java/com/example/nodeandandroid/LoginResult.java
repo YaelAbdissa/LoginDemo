2708c6d449487d258c54441396d122245a79868b
eyael <eyaelabdissa8@gmail.com>
2020-09-23 15:26:36 +0300

    first commit

---
+++ app/src/main/java/com/example/nodeandandroid/LoginResult.java
@@ -0,0 +1,17 @@
+package com.example.nodeandandroid;
+
+public class LoginResult {
+
+    private String name;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+}
